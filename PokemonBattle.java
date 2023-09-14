6fdf4bb9c25b6060e8eedf72ed0b88d131f2450a
Soham Pati <sohampati2005@gmail.com>
2023-09-14 19:15:41 -0400

    HTML and css FILES

--- PokemonBattle.java
+++ PokemonBattle.java
@@ -7,8 +7,19 @@ public class PokemonBattle{
 public static void main(String[] Args){
 
 	Random rand = new Random();
-	Scanner scan = new Scanner();
-	
+	Scanner scan = new Scanner(System.in);
+	String nickname;
+	String rivalNickname;
+	double rivalHealth = (double) (rand.nextInt(20) + 20);
+	System.out.println("Enter your Pokemon's nickname: ");
+	nickname = scan.nextLine();
+	nickname.trim();
+	System.out.println("Enter your rival Pokemon's nickname: ");
+	rivalNickname = scan.nextLine();
+	rivalNickname.trim();
+	System.out.printf("Your rival has chosen %S to fight, which has %D health.", rivalNickname, rivalHealth);
+
+
 
 
 
